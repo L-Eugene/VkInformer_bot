cd884e328cb3552c31c55a19813c8ae627714e11
Eugene Lapeko <eugene@lapeko.info>
2018-10-11 15:18:15 +0300

    Test specs for Vk::Tlg

---
+++ spec/unit_test/telegram/vk_informer_classes_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Vk::Tlg do
+  it 'should escape messages right' do
+    expect(Vk::Tlg.escape 'domainXsed3').to eq 'domainXsed3'
+    expect(Vk::Tlg.escape '*domain*Xsed3').to eq '\*domain\*Xsed3'
+    expect(Vk::Tlg.escape '_domain_Xsed3').to eq '\_domain\_Xsed3'
+    expect(Vk::Tlg.escape '*_domain_*Xsed3').to eq '\*\_domain\_\*Xsed3'
+  end
+end
