c11c3c2f76f2e8aff8a5d197300657fe0c82fc4c
Eugene Lapeko <eugene@lapeko.info>
2018-10-11 15:21:23 +0300

    Rubocop fix

--- spec/unit_test/telegram/vk_informer_classes_spec.rb
+++ spec/unit_test/telegram/vk_informer_classes_spec.rb
@@ -4,9 +4,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Vk::Tlg do
   it 'should escape messages right' do
-    expect(Vk::Tlg.escape 'domainXsed3').to eq 'domainXsed3'
-    expect(Vk::Tlg.escape '*domain*Xsed3').to eq '\*domain\*Xsed3'
-    expect(Vk::Tlg.escape '_domain_Xsed3').to eq '\_domain\_Xsed3'
-    expect(Vk::Tlg.escape '*_domain_*Xsed3').to eq '\*\_domain\_\*Xsed3'
+    expect(Vk::Tlg.escape('domainXsed3')).to eq 'domainXsed3'
+    expect(Vk::Tlg.escape('*domain*Xsed3')).to eq '\*domain\*Xsed3'
+    expect(Vk::Tlg.escape('_domain_Xsed3')).to eq '\_domain\_Xsed3'
+    expect(Vk::Tlg.escape('*_domain_*Xsed3')).to eq '\*\_domain\_\*Xsed3'
   end
 end
