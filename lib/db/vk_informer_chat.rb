157be1e970ab99a9d66e0e1bb2879330d273bfa2
Eugene Lapeko <eugene@lapeko.info>
2018-10-28 21:20:15 +0300

    Ignore non-hash responses.

--- lib/db/vk_informer_chat.rb
+++ lib/db/vk_informer_chat.rb
@@ -101,7 +101,7 @@ module Vk
       Vk.log.info t.chat.sending(message: post.message_id, chat: chat_id)
       post.data.each do |p|
         response = send(p.use_method, p.to_hash)
-        p.result response.dig('result')
+        p.result response.dig('result') if response.is_a? Hash
       end
     end
 
