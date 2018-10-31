697701c36d9ca3e17e87110685eaa48a9918c14c
Eugene Lapeko <eugene@lapeko.info>
2018-10-31 12:21:57 +0300

    Use wrappers to execute bundler

--- deploy/development.sh
+++ deploy/development.sh
@@ -7,9 +7,8 @@ cat >/tmp/upgrade_script_dbg.sh <<EOF
 
   # UPDATE GEMSET
   cd $VK_INFORMER_DEBUG_BOTSERVER_PATH
-  rvm use $VK_INFORMER_DEBUG_RVM
-  bundle install
-  bundle update
+  $VK_INFORMER_DEBUG_RVM/bundle install
+  $VK_INFORMER_DEBUG_RVM/bundle update
 
   # RESTART BOTSERVER
   sudo service $BOTSERVER_SERVICE_DEBUG restart
@@ -22,7 +21,7 @@ ssh $DEPLOY_USER@$DEPLOY_SERVER '[ -f upgrade_script_dbg.sh ] && rm upgrade_scri
 scp /tmp/upgrade_script_dbg.sh $DEPLOY_USER@$DEPLOY_SERVER:upgrade_script_dbg.sh
 
 # Execute upgrade script
-ssh $DEPLOY_USER@$DEPLOY_SERVER /bin/bash -l ./upgrade_script_dbg.sh
+ssh $DEPLOY_USER@$DEPLOY_SERVER /bin/bash ./upgrade_script_dbg.sh
 
 # Remove upgrade script from server
 ssh $DEPLOY_USER@$DEPLOY_SERVER '[ -f upgrade_script_dbg.sh ] && rm upgrade_script_dbg.sh'
