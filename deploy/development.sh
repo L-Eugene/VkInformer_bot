9049cb66f7b08e197ea2efacd884914f2d47dcfb
Eugene Lapeko <eugene@lapeko.info>
2018-10-31 11:02:55 +0300

    Use Travis to deploy development version to server.

---
+++ deploy/development.sh
@@ -0,0 +1,26 @@
+cat >/tmp/upgrade_script_dbg.sh <<EOF
+  # UPDATE SOURCE
+  cd $VK_INFORMER_DEBUG_PATH
+  git pull
+
+  # UPDATE GEMSET
+  cd $VK_INFORMER_DEBUG_BOTSERVER_PATH
+  rmv use $VK_INFORMER_DEBUG_RVM
+  bundle install
+  bundle update
+
+  # RESTART BOTSERVER
+  sudo service $BOTSERVER_SERVICE_DEBUG restart
+EOF
+
+# Remove old version of upgrade script
+ssh $DEPLOY_SERVER '[ -f upgrade_script_dbg.sh ] && rm upgrade_script_dbg.sh'
+
+# Copy upgrade script to server
+scp /tmp/upgrade_script_dbg.sh $DEPLOY_SERVER:upgrade_script_dbg.sh
+
+# Execute upgrade script
+ssh $DEPLOY_SERVER /bin/bash ./upgrade_script_dbg.sh
+
+# Remove upgrade script from server
+ssh $DEPLOY_SERVER '[ -f upgrade_script_dbg.sh ] && rm upgrade_script_dbg.sh'
