ffe93fd3d908e04cf74161aa5905a0ad93a38faa
Eugene Lapeko <eugene@lapeko.info>
2018-10-31 14:31:52 +0300

    Build should fail if upgrade script was unsuccessful.

--- deploy/development.sh
+++ deploy/development.sh
@@ -24,7 +24,7 @@ ssh $DEPLOY_USER@$DEPLOY_SERVER '[ -f upgrade_script_dbg.sh ] && rm upgrade_scri
 scp /tmp/upgrade_script_dbg.sh $DEPLOY_USER@$DEPLOY_SERVER:upgrade_script_dbg.sh
 
 # Execute upgrade script
-ssh $DEPLOY_USER@$DEPLOY_SERVER /bin/bash -l ./upgrade_script_dbg.sh
+ssh $DEPLOY_USER@$DEPLOY_SERVER /bin/bash ./upgrade_script_dbg.sh
 DEPLOY_STATUS=$?
 
 # Remove upgrade script from server
