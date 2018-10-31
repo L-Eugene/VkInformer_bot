db05e082eb9eb803278ca81487b4125548a5297c
Eugene Lapeko <eugene@lapeko.info>
2018-10-31 12:57:07 +0300

    Added output in deploymend script.

--- deploy/development.sh
+++ deploy/development.sh
@@ -2,15 +2,18 @@
 
 cat >/tmp/upgrade_script_dbg.sh <<EOF
   # UPDATE SOURCE
+  echo "Updating source from Git"
   cd $VK_INFORMER_DEBUG_PATH
   git pull
 
   # UPDATE GEMSET
+  echo "Updating bundle"
   cd $VK_INFORMER_DEBUG_BOTSERVER_PATH
   $VK_INFORMER_DEBUG_RVM/bundle install --without test
   $VK_INFORMER_DEBUG_RVM/bundle update
 
   # RESTART BOTSERVER
+  echo "Restarting server"
   sudo service $BOTSERVER_SERVICE_DEBUG restart
 EOF
 
